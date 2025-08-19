ba7b044f68983bf4b8d99cc3425483f1587c5299
hjimin027 <hjimin027@gmail.com>
2025-08-19 16:38:24 +0900

    column_practice.dart 추가

--- lib/screen/button_screen.dart
+++ lib/screen/button_screen.dart
@@ -1,3 +1,5 @@
+import 'package:contact/screen/column/column_practice.dart';
+import 'package:contact/screen/column/column_screen.dart';
 import 'package:contact/screen/container/container_practice_screen.dart';
 import 'package:contact/screen/container/container_screen.dart';
 import 'package:flutter/material.dart';
@@ -23,6 +25,18 @@ class ButtonScreen extends StatelessWidget {
                 return ContainerPracticeScreen();
               },));
             }, child: Text("Container Practice")),
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return ColumnScreen();
+              },));
+            }, child: Text("Column")),
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return ColumnPractice();
+              },));
+            }, child: Text("Column_Practice")),
           ],
         ),
       ),
---
+++ lib/screen/column/column_practice.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class ColumnPractice extends StatelessWidget {
+  const ColumnPractice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Column Practice'),),
+      body: Container(
+        width: 300,
+        color: Colors.purple,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: 100, height: 100,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 100, height: 100,
+              color: Colors.red,
+            ),
+            Container(
+              width: 100, height: 100,
+              color: Colors.green,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
