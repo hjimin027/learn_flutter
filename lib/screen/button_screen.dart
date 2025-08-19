8da1b6aaa9e752e025ac383fc941ea67c6e3f589
hjimin027 <hjimin027@gmail.com>
2025-08-19 16:05:31 +0900

    Initial commit

---
+++ lib/screen/button_screen.dart
@@ -0,0 +1,31 @@
+import 'package:contact/screen/container/container_practice_screen.dart';
+import 'package:contact/screen/container/container_screen.dart';
+import 'package:flutter/material.dart';
+
+class ButtonScreen extends StatelessWidget {
+  const ButtonScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ElevatedButton(onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return ContainerScreen();
+              },));
+            }, child: Text("Container")),
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return ContainerPracticeScreen();
+              },));
+            }, child: Text("Container Practice")),
+          ],
+        ),
+      ),
+    );
+  }
+}
