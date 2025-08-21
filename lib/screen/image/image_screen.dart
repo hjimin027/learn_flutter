7da79cf280980321b7c1343a4d4d7a5cb58781e8
hjimin027 <hjimin027@gmail.com>
2025-08-21 13:37:52 +0900

    image 연습

--- lib/screen/image/image_screen.dart
+++ lib/screen/image/image_screen.dart
@@ -6,7 +6,7 @@ class ImageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Image"),),
+      appBar: AppBar(title: Text("Image")),
       body: SizedBox(
         width: double.infinity,
         child: SingleChildScrollView(
@@ -14,40 +14,48 @@ class ImageScreen extends StatelessWidget {
             children: [
               Container(
                 color: Colors.grey,
-                width: 200, height: 200,
+                width: 300,
+                height: 300,
                 child: Image.network('https://sailing-it-images.s3.ap-northeast-2.amazonaws.com/company/company_device.png'),
               ),
-
-              SizedBox(height: 30,),
-
+              SizedBox(height: 30),
               Container(
                 color: Colors.grey,
-                width: 200, height: 200,
-                child: Image.network('https://sailing-it-images.s3.ap-northeast-2.amazonaws.com/company/company_device.png',
-                // https 에러 발생. web security 때문에 크롬으로 실행해서. jinhan38.com/168 티스토리. 에뮬레이터 실행하면 잘 될 거임.
-                fit: BoxFit.fitHeight,)
+                width: 300,
+                height: 300,
+                child: Image.network(
+                  'https://sailing-it-images.s3.ap-northeast-2.amazonaws.com/company/company_device.png',
+                  fit: BoxFit.fitHeight,
+                ),
               ),
 
-              SizedBox(height: 30,),
-
+              SizedBox(height: 30),
               Container(
-                  color: Colors.grey,
-                  width: 200, height: 200,
-                  child: Image.network('https://sailing-it-images.s3.ap-northeast-2.amazonaws.com/company/company_device.png',
-                    // https 에러 발생. web security 때문에 크롬으로 실행해서. jinhan38.com/168 티스토리. 에뮬레이터 실행하면 잘 될 거임.
-                    fit: BoxFit.fitWidth,)
+                color: Colors.grey,
+                width: 300,
+                height: 300,
+                child: Image.network(
+                  'https://sailing-it-images.s3.ap-northeast-2.amazonaws.com/company/company_device.png',
+                  fit: BoxFit.fitWidth,
+                ),
               ),
 
-              SizedBox(height: 30,),
-
+              SizedBox(height: 30),
               Container(
-                  color: Colors.grey,
-                  width: 200, height: 200,
-                  child: Image.network('https://sailing-it-images.s3.ap-northeast-2.amazonaws.com/company/company_device.png',
-                    // https 에러 발생. web security 때문에 크롬으로 실행해서. jinhan38.com/168 티스토리. 에뮬레이터 실행하면 잘 될 거임.
-                    fit: BoxFit.cover,
-                  alignment: Alignment.topRight,)
-              )
+                color: Colors.grey,
+                width: 200,
+                height: 200,
+                child: Image.network(
+                  'https://sailing-it-images.s3.ap-northeast-2.amazonaws.com/company/company_device.png',
+                  fit: BoxFit.cover,
+                  alignment: Alignment.topLeft,
+                ),
+              ),
+              SizedBox(height: 30),
+
+              SizedBox(width: 150, height: 150, child: Image.asset('assets/company_device.png')),
+
+              SizedBox(height: 30),
             ],
           ),
         ),
