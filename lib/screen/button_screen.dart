a262c6467873a406b8f89b98c75a437c582c4ca3
hjimin027 <hjimin027@gmail.com>
2025-08-21 14:11:30 +0900

    scrollview

--- lib/screen/button_screen.dart
+++ lib/screen/button_screen.dart
@@ -11,6 +11,8 @@ import 'package:contact/screen/stack/stack_practice_screen.dart';
 import 'package:contact/screen/stack/stack_screen.dart';
 import 'package:contact/screen/text/text_practice.dart';
 import 'package:contact/screen/text/text_screen.dart';
+import 'package:contact/scrollview/scrollview_practice_screen.dart';
+import 'package:contact/scrollview/scrollview_screen.dart';
 import 'package:flutter/material.dart';
 
 class ButtonScreen extends StatelessWidget {
@@ -19,97 +21,115 @@ class ButtonScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ElevatedButton(onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return ContainerScreen();
-              },));
-            }, child: Text("Container")),
-            SizedBox(height: 10),
-            ElevatedButton(onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return ContainerPracticeScreen();
-              },));
-            }, child: Text("Container Practice")),
-            SizedBox(height: 10),
-            ElevatedButton(onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return ColumnScreen();
-              },));
-            }, child: Text("Column")),
-            SizedBox(height: 10),
-            ElevatedButton(onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return ColumnPracticeScreen();
-              },));
-            }, child: Text("Column_Practice")),
-            SizedBox(height: 10),
-            ElevatedButton(onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return RowScreen();
-              },));
-            }, child: Text("Row")),
-            SizedBox(height: 10),
-            ElevatedButton(onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return RowPracticeScreen();
-              },));
-            }, child: Text("Row Practice")),
-            SizedBox(height: 10),
-            ElevatedButton(onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return ColumnRowPracticeScreen();
-              },));
-            }, child: Text("Column Row Practice")),
-            SizedBox(height: 10),
-            SizedBox(height: 10),
-            SizedBox(height: 10),
-            ElevatedButton(onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return TextScreen();
-              },));
-            }, child: Text("Text")),
-            SizedBox(height: 10),
-            ElevatedButton(onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return TextPractice();
-              },));
-            }, child: Text("Text Practice")),
-            SizedBox(height: 10),
-            SizedBox(height: 10),
-            SizedBox(height: 10),
+      resizeToAvoidBottomInset: false,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ElevatedButton(onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return ContainerScreen();
+                },));
+              }, child: Text("Container")),
+              SizedBox(height: 10),
+              ElevatedButton(onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return ContainerPracticeScreen();
+                },));
+              }, child: Text("Container Practice")),
+              SizedBox(height: 10),
+              ElevatedButton(onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return ColumnScreen();
+                },));
+              }, child: Text("Column")),
+              SizedBox(height: 10),
+              ElevatedButton(onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return ColumnPracticeScreen();
+                },));
+              }, child: Text("Column_Practice")),
+              SizedBox(height: 10),
+              ElevatedButton(onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return RowScreen();
+                },));
+              }, child: Text("Row")),
+              SizedBox(height: 10),
+              ElevatedButton(onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return RowPracticeScreen();
+                },));
+              }, child: Text("Row Practice")),
+              SizedBox(height: 10),
+              ElevatedButton(onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return ColumnRowPracticeScreen();
+                },));
+              }, child: Text("Column Row Practice")),
+              SizedBox(height: 10),
+              SizedBox(height: 10),
+              SizedBox(height: 10),
+              ElevatedButton(onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return TextScreen();
+                },));
+              }, child: Text("Text")),
+              SizedBox(height: 10),
+              ElevatedButton(onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return TextPractice();
+                },));
+              }, child: Text("Text Practice")),
+              SizedBox(height: 10),
+              SizedBox(height: 10),
+              SizedBox(height: 10),
 
-            ElevatedButton(onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return ImageScreen();
-              },));
-            }, child: Text("Image")),
-            SizedBox(height: 10),
-            ElevatedButton(onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return ImagePracticeScreen();
-              },));
-            }, child: Text("Image Practice")),
+              ElevatedButton(onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return ImageScreen();
+                },));
+              }, child: Text("Image")),
+              SizedBox(height: 10),
+              ElevatedButton(onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return ImagePracticeScreen();
+                },));
+              }, child: Text("Image Practice")),
 
-            SizedBox(height: 10),
-            SizedBox(height: 10),
-            SizedBox(height: 10),
+              SizedBox(height: 10),
+              SizedBox(height: 10),
+              SizedBox(height: 10),
 
-            ElevatedButton(onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return StackScreen();
-              },));
-            }, child: Text("Stack")),
-            ElevatedButton(onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return StackPracticeScreen();
-              },));
-            }, child: Text("Stack Practice")),
-          ],
+              ElevatedButton(onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return StackScreen();
+                },));
+              }, child: Text("Stack")),
+              ElevatedButton(onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return StackPracticeScreen();
+                },));
+              }, child: Text("Stack Practice")),
+              SizedBox(height: 10),
+              SizedBox(height: 10),
+              SizedBox(height: 10),
 
+              ElevatedButton(onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return ScrollviewScreen();
+                },));
+              }, child: Text("Scrollview")),
+              SizedBox(height: 10),
+              ElevatedButton(onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return ScrollviewPracticeScreen();
+                },));
+              }, child: Text("Scrollview Practice")),
+            ],
+
+          ),
         ),
       ),
     );
---
+++ lib/scrollview/scrollview_practice_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ScrollviewPracticeScreen extends StatelessWidget {
+  const ScrollviewPracticeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
